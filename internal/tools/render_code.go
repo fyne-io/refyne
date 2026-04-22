8ac9ef038d20dc9baa47a343e91e65169f18b6fa
Simon Dassow <simon@fabams.nl>
2026-04-22 13:24:43 +0200

    Document exported method

--- internal/tools/render_code.go
+++ internal/tools/render_code.go
@@ -5,6 +5,8 @@ import (
 	"text/template"
 )
 
+// RenderCode takes a template string and any given data to render it, and
+// returns the resulting string and possible error.
 func RenderCode(text string, data any) (string, error) {
 	tmpl, err := template.New("").Parse(text)
 	if err != nil {
